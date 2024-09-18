26b88077f1f765741d4fabc2ee6d01ba105e43b4
Esmaeil Sheikhi <es.sheikhi@gmail.com>
2024-09-18 13:13:43 +0330

    Adding Tax Calculator Project Adding City Repository

---
+++ TaxCalculator/Repositories/CityRepository.cs
@@ -0,0 +1,50 @@
+﻿using DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator.Repositories
+{
+    public class CityRepository : ICityRepository
+    {
+        private readonly CityTaxContext _taxContext;
+        public CityRepository(CityTaxContext taxContext)
+        {
+            _taxContext = taxContext;
+        }
+        public List<City> GetAllCities()
+        {
+            List<City> cities = _taxContext.Cities
+                .Include(c => c.Fees)
+                .Include(c => c.FreeDays)
+                .Include(c => c.PublicHolidays)
+                .ToList();
+            return cities;
+        }
+
+        public City GetCityById(int id)
+        {
+            City city = _taxContext.Cities
+                .Where(c => c.Id == id)
+                .Include(c => c.Fees)
+                .Include(c => c.FreeDays)
+                .Include(c => c.PublicHolidays)
+                .FirstOrDefault();
+            return city;
+        }
+
+        public City GetCityByName(string name)
+        {
+            City? city = _taxContext.Cities
+                .Where(c => c.Name == name)
+                .Include(c => c.Fees)
+                .Include(c => c.FreeDays)
+                .Include(c => c.PublicHolidays)
+                .FirstOrDefault();
+            return city;
+        }
+    }
+}
